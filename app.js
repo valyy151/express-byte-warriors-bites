edaf2acc3c46f913d068e352a832f1a163d00d2d
valyy151 <marin.valenta1@gmail.com>
2023-03-15 12:23:26 +0100

    Added layout

--- app.js
+++ app.js
@@ -8,16 +8,21 @@ app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
 const pizzas = [
-	{ title: 'Margarita', price: '€10', image: '../images/margarita.jpg' },
-	{ title: 'Veggie', price: '€12', image: '../images/veggie.jpg' },
-	{ title: 'Seafood', price: '€15', image: '../images/seafood.jpg' },
+	{
+		title: 'Margarita',
+		price: '€10',
+		image: '/images/margarita.jpg',
+		ingredients: ['Mozzarella', 'Tomato-sauce', 'Basilicum'],
+	},
+	{ title: 'Veggie', price: '€12', image: '/images/veggie.jpg' },
+	{ title: 'Seafood', price: '€15', image: '/images/seafood.jpg' },
 ];
 
 const [margarita, veggie, seafood] = pizzas;
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/views/home.html'));
-app.get('/contact', (req, res) => res.sendFile(__dirname + '/views/contact.html'));
-app.get('/pizzas', (req, res) => res.sendFile(__dirname + '/views/pizzas.html'));
+app.get('/', (req, res) => res.render('home'));
+app.get('/contact', (req, res) => res.render('contact'));
+app.get('/pizzas', (req, res) => res.render('pizzas'));
 app.get('/pizzas/margarita', (req, res) => res.render('product', margarita));
 app.get('/pizzas/veggie', (req, res) => res.render('product', veggie));
 app.get('/pizzas/seafood', (req, res) => res.render('product', seafood));
