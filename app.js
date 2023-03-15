bd4c50d689b991e6f53fa021ba66a66a71b5155e
valyy151 <marin.valenta1@gmail.com>
2023-03-15 09:53:46 +0100

    More routes added

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/views/home.html'));
+app.get('/contact', (req, res) => res.sendFile(__dirname + '/views/contact.html'));
+app.get('/pizzas', (req, res) => res.sendFile(__dirname + '/views/pizzas.html'));
+app.get('/pizzas/margarita', (req, res) => res.sendFile(__dirname + '/views/margarita.html'));
+app.get('/pizzas/veggie', (req, res) => res.sendFile(__dirname + '/views/veggie.html'));
+app.get('/pizzas/seafood', (req, res) => res.sendFile(__dirname + '/views/seafood.html'));
 
 app.listen(3000, () => console.log('Listening to port 3000'));
