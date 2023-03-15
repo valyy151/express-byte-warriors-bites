820aeb67a612615fadfceba88572d7e8b10d9035
valyy151 <marin.valenta1@gmail.com>
2023-03-15 11:12:58 +0100

    Handlebars rendering

--- app.js
+++ app.js
@@ -2,13 +2,24 @@ const express = require('express');
 
 const app = express();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+
 app.use(express.static('public'));
+
+const pizzas = [
+	{ title: 'Margarita', price: '€10', image: '../images/margarita.jpg' },
+	{ title: 'Veggie', price: '€12', image: '../images/veggie.jpg' },
+	{ title: 'Seafood', price: '€15', image: '../images/seafood.jpg' },
+];
+
+const [margarita, veggie, seafood] = pizzas;
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/home.html'));
 app.get('/contact', (req, res) => res.sendFile(__dirname + '/views/contact.html'));
 app.get('/pizzas', (req, res) => res.sendFile(__dirname + '/views/pizzas.html'));
-app.get('/pizzas/margarita', (req, res) => res.sendFile(__dirname + '/views/margarita.html'));
-app.get('/pizzas/veggie', (req, res) => res.sendFile(__dirname + '/views/veggie.html'));
-app.get('/pizzas/seafood', (req, res) => res.sendFile(__dirname + '/views/seafood.html'));
+app.get('/pizzas/margarita', (req, res) => res.render('product', margarita));
+app.get('/pizzas/veggie', (req, res) => res.render('product', veggie));
+app.get('/pizzas/seafood', (req, res) => res.render('product', seafood));
 
-app.listen(3000, () => console.log('Listening to port 3000'));
+app.listen(3000, () => console.log('Server started successfuly on port 3000'));
